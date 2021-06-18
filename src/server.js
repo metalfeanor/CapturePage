5ea73b222f17e02fd958d5cfb1977d9ab43ddf32
Christophe <cnamoune@gmail.com>
2021-06-18 20:56:06 +0200

    testing new code

--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+/*
 const express = require("express");
 const serverless = require("serverless-http");
 const nodemailer = require("nodemailer");
@@ -45,6 +46,7 @@ app.post("/", (req, res) => {
 });
 
 app.use(`/.netlify/functions/server`, router);
+*/
 //app.use("/", (req, res) => res.sendFile(__dirname + "../public/index.html"));
 
 /*app.get("/", (req, res) => {
@@ -83,7 +85,56 @@ app.post("/", (req, res) => {
 app.listen(PORT, () => {
   console.log(`server running on PORT ${PORT}`);
 });*/
-
+/*
 module.exports = app;
 
 module.exports.handler = serverless(app);
+*/
+const nodemailer = require("nodemailer");
+
+exports.handler = async function (event, context, callback) {
+  // Parse the JSON text received.
+  const body = JSON.parse(event.body);
+
+  // Build an HTML string to represent the body of the email to be sent.
+  const html = `<div style="margin: 20px auto;">test</div>`;
+
+  // Generate test SMTP service account from ethereal.email. Only needed if you
+  // don't have a real mail account for testing
+  //let testAccount = await nodemailer.createTestAccount();
+
+  // create reusable transporter object using the default SMTP transport
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "nam.testeur@gmail.com",
+      pass: "pass@_Axn,fju",
+    },
+  });
+
+  const mailOptions = {
+    from: "nam.testeur@gmail.com",
+    to: body.email,
+    subject: `Recrutement : ${body.first} ${body.last}`,
+    text: "test",
+  };
+
+  try {
+    // send mail with defined transport object
+    let info = await transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        //console.log(error);
+        res.send("error");
+      } else {
+        //console.log("email sent " + info.response);
+        res.send("success");
+      }
+    });
+    // Log the result
+    console.log(info);
+    callback(null, { statusCode: 200, body: JSON.stringify(info) });
+  } catch (error) {
+    // Catch and log error.
+    callback(error);
+  }
+};
